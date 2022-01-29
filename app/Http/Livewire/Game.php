98cd42cb0283c9a9744a77c70b7ac6f46a02be62
Alexander <alexanderjoemc@gmail.com>
2022-01-29 14:34:14 -0500

    base game working

--- app/Http/Livewire/Game.php
+++ app/Http/Livewire/Game.php
@@ -6,23 +6,70 @@ use Livewire\Component;
 
 class Game extends Component
 {
-    public $store = [];
-    public $solution = ['T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 't', 'e', 's', 't'];
-    public $solutionLength = 11;
+    public $solution = ['t', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 't', 'e', 's', 't'];
+    public $curPos = 0;
     public $attempts = 0;
-    private $MAX_ATTEMPTS = 6;
+    public $MAX_ATTEMPTS = 6;
+    public $storeInput;
+    public $attemptsStore = [];
+    public $solved = false;
 
     public function render()
     {
         return view('livewire.game');
     }
 
-    public function handleKey($key)
+    public function getStoreProperty()
     {
-        if ($key == "Backspace") {
-            array_pop($this->store);
-        } else if (!preg_match('/^[A-Za-z0-9]+$/', $key)){
-            $this->store[] = $key;
+        return str_split($this->storeInput);
+    }
+
+    public function getSolutionLengthProperty()
+    {
+        return count($this->solution);
+    }
+
+    public function getKey($index, $attempt)
+    {
+        if ($attempt == $this->attempts) {
+            if (array_key_exists($index, $this->getStoreProperty())) {
+                return $this->getStoreProperty()[$index];
+            }
+        } else {
+            if (isset($this->attemptsStore[$attempt])) {
+                if (array_key_exists($index, $this->attemptsStore[$attempt]))
+                    return $this->attemptsStore[$attempt][$index];
+            }
+        }
+
+        return '';
+    }
+
+    public function getColor($index, $attempt)
+    {
+        if (isset($this->attemptsStore[$attempt])) {
+            if (array_key_exists($index, $this->attemptsStore[$attempt]))
+                if ($this->solution[$index] == $this->attemptsStore[$attempt][$index]) {
+                    return 'bg-green-500';
+                } else if (in_array($this->attemptsStore[$attempt][$index], $this->solution)) {
+                    return 'bg-yellow-400';
+                }
+        }
+
+        return '';
+    }
+
+    public function handleAttempt()
+    {
+        if ($this->getSolutionLengthProperty() == count($this->getStoreProperty())) {
+            $this->attemptsStore[] = $this->getStoreProperty();
+            $this->attempts++;
+            if($this->getStoreProperty() == $this->solution) {
+                $this->solved = true;
+            }
+            $this->storeInput = "";
+        } else {
+            session()->flash('error', 'Your solution is not the correct length!');
         }
     }
 }
--- resources/views/livewire/game.blade.php
+++ resources/views/livewire/game.blade.php
@@ -2,19 +2,23 @@
     <h1 class="text-5xl text-white uppercase tracking-wider py-6">Phrasele</h1>
     <div class="pt-4">
         <div>
-            <div class="flex">
-                @foreach($solution as $letter)
-                    @if($letter == " ")
-                        <div class="w-10 h-10 px-0.5 mx-1"></div>
-                    @else
-                    <div class="inline-flex justify-center content-center items-center font-bold uppercase border border-2 border-gray-100 w-12 h-12 mx-1 text-white text-2xl">
-                        {{ "" }}
-                    </div>
-                    @endif
-                @endforeach
-            </div>
-{{--            @json($store)--}}
-            <input type="text" wire:keydown="handleKey($event.key)" class="opacity-0" autofocus>
+            @for($i = 0; $i < $MAX_ATTEMPTS; $i++)
+                <div class="flex mt-2">
+                    @foreach($solution as $index => $letter)
+                        @if($letter == " ")
+                            <div class="w-10 h-10 px-0.5 mx-1"></div>
+                        @else
+                            <div class="inline-flex justify-center content-center items-center font-bold uppercase border border-2 border-gray-100 w-12 h-12 mx-1 text-white text-2xl {{ $this->getColor($index, $i) }}">
+                                {{ $this->getKey($index, $i) }}
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+            @endfor
+            @if(!$solved)
+                <input type="text" wire:model="storeInput" wire:keydown.enter="handleAttempt" class="opacity-100 mt-4" autofocus>
+            @endif
+            <p class="bg-green-500 bg-yellow-400 hidden"></p>
         </div>
     </div>
 </div>
